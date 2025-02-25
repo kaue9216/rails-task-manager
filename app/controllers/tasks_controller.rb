49a01f1d85aea2b24361859012ecafe327e56a97
kaue ogibowski <kaueogibowski@outlook.com>
2025-02-25 17:03:12 -0300

    no CSS site

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    create = params.require(:task).permit(:title, :details)
+    @task = Task.new(create)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    update = params.require(:task).permit(:title, :details)
+    @task.update(update)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+end
